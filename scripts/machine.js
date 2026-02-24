56930fef5b00eadb5302104c695332f32826c75e
AforArdi <binsbro36@gmail.com>
2026-02-24 14:21:36 +0600

    button toggle done for interview section, card is adding up in interview section, and hidden working properly

--- scripts/machine.js
+++ scripts/machine.js
@@ -15,34 +15,35 @@ function clickStyle(id){
     selectedBtn.classList.add('btn-primary');
 
     // only showing the selected job card
-    // if (id == 'interviewFilterBtn'){
-    //     allJobsSection.classList.add('hidden');
-    //     filteredJobsSection.classList.remove('hidden');
-    // }
+    if (id === 'btn-interview') {
+        allJobsSection.classList.add('hidden');
+        filteredJobsSection.classList.remove('hidden');
+    } else if (id === 'btn-all') {
+        allJobsSection.classList.remove('hidden');
+        filteredJobsSection.classList.add('hidden');
+    }
 }
 
 function renderInterview(){
     filteredJobsSection.innerHTML = '';
 
     for (let interview of interviewList){
-        let div = document.createElement('div');
+        const div = document.createElement('div');
         div.className = 'flex justify-between border p-6 rounded-md';
         // div.classList.add('flex', 'justify-between', 'border', 'p-6', 'rounded-md');
 
         div.innerHTML = `
-        <!-- card 1 -->
-             <div class="flex justify-between border p-6 rounded-md">
                 <!-- main part -->
                 <div class="space-y-4">
                     <!-- part 1 -->
                     <div class="flex flex-col gap-2">
-                        <p class="company-name">Mobile First Corp</p>
-                        <p class="job-position">React Native Developer</p>
+                        <p class="company-name">${interview.companyName}</p>
+                        <p class="job-position">${interview.jobName}</p>
                     </div>
-                    <p>Remote &bull; Full-time &bull; $130,000 - $175,000</p>
+                    <p>${interview.jobOffers}</p>
                     <div class="flex flex-col gap-2">
-                        <p class="applied-status">Not Applied</p>
-                        <p>Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+                        <p class="applied-status">${interview.status}</p>
+                        <p>${interview.jobInfoPara}</p>
                     </div>
                     <!-- part 2 -->
                      <div class="flex gap-2">
@@ -54,7 +55,6 @@ function renderInterview(){
                 <div>
                     <button class="btn"><i class="fa-solid fa-trash-can"></i></button>
                 </div>
-             </div>
         `
         filteredJobsSection.appendChild(div);
     }
--- scripts/script.js
+++ scripts/script.js
@@ -26,24 +26,33 @@ mainContainer.addEventListener('click', function(e){
 
         const companyName = parentNode.querySelector('.company-name').textContent;
         const jobName = parentNode.querySelector('.job-position').textContent;
+        const jobOffers = parentNode.querySelector('.job-offers').textContent;
         const status = parentNode.querySelector('.applied-status').textContent;
-    
+        const jobInfoPara = parentNode.querySelector('.job-info-para').textContent;
+
+        // status
+        parentNode.querySelector('.applied-status').innerText = 'Applied';
+
         const JobCardInfo = {
             companyName,
             jobName,
-            status
+            jobOffers,
+            status: 'Applied',
+            jobInfoPara
         }
         
         // checking if item exists in the array ir not
         const companyExists = interviewList.find(item => item.companyName == JobCardInfo.companyName);
-    
-        // status
-        parentNode.querySelector('.applied-status').innerText = 'Applied';
+        
         if(!companyExists){
             interviewList.push(JobCardInfo);
         }
+        
+        // update count
+        calculateCount();
+
         // console.log(interviewList);
-        renderInterview();
+        renderInterview()
     }
 
 })
