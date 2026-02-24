ef7e927b84823c08ab912b7e67a6e1a2f86b84b9
AforArdi <binsbro36@gmail.com>
2026-02-24 12:31:21 +0600

    interview code only showing on interview btn clicked, need to append the code now

--- scripts/machine.js
+++ scripts/machine.js
@@ -1,3 +1,53 @@
+// total count on dashboard function
 function calculateCount(){
     totalJob.innerText = allJobsSection.children.length;
+    totalInterview.innerText = interviewList.length;
+    totalRejected.innerText = rejectedList.length;
+}
+
+// btn toggling color change function
+function clickStyle(id){
+    allFilterBtn.classList.remove('btn-primary');
+    interviewFilterBtn.classList.remove('btn-primary');
+    rejectedFilterBtn.classList.remove('btn-primary');
+
+    const selectedBtn = document.getElementById(id);
+    selectedBtn.classList.add('btn-primary');
+}
+
+function renderInterview(){
+    filteredJobsSection.innerHTML = '';
+    for (let interview of interviewList){
+        let div = document.createElement('div');
+        div.callName = 'flex justify-between border p-6 rounded-md';
+        // div.classList.add('flex', 'justify-between', 'border', 'p-6', 'rounded-md');
+
+        div.innerHTML = `
+        <!-- card 1 -->
+             <div class="flex justify-between border p-6 rounded-md">
+                <!-- main part -->
+                <div class="space-y-4">
+                    <!-- part 1 -->
+                    <div class="flex flex-col gap-2">
+                        <p class="company-name">Mobile First Corp</p>
+                        <p class="job-position">React Native Developer</p>
+                    </div>
+                    <p>Remote &bull; Full-time &bull; $130,000 - $175,000</p>
+                    <div class="flex flex-col gap-2">
+                        <p class="applied-status">Not Applied</p>
+                        <p>Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+                    </div>
+                    <!-- part 2 -->
+                     <div class="flex gap-2">
+                        <button class="btn btn-interview-stat">INTERVIEW</button>
+                        <button class="btn btn-rejected-stat">REJECTED</button>
+                     </div>
+                </div>
+                <!-- delete part -->
+                <div>
+                    <button class="btn"><i class="fa-solid fa-trash-can"></i></button>
+                </div>
+             </div>
+        `
+    }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,48 @@
 let totalJob = document.getElementById('total-dashboard');
 let totalInterview = document.getElementById('interview');
 let totalRejected = document.getElementById('rejected');
+
+
 const allJobsSection = document.getElementById('allJobs');
+const mainContainer = document.querySelector('main');
+const filteredJobsSection = document.getElementById('filtered-jobs');
+
+const allFilterBtn = document.getElementById('btn-all');
+const interviewFilterBtn = document.getElementById('btn-interview');
+const rejectedFilterBtn = document.getElementById('btn-rejected');
+
+// empty array for other toggle
+let interviewList = [];
+let rejectedList = [];
 
 calculateCount();
+
+
+mainContainer.addEventListener('click', function(e){
+
+    console.log(e.target.classList.contains('btn-interview-stat'))
+    // fire this only for interview btn
+    if (e.target.classList.contains('btn-interview-stat')){
+        const parentNode = e.target.parentNode.parentNode;
+        const companyName = parentNode.querySelector('.company-name').textContent;
+        const jobName = parentNode.querySelector('.job-position').textContent;
+        const status = parentNode.querySelector('.applied-status').textContent;
+    
+        const JobCardInfo = {
+            companyName,
+            jobName,
+            status
+        }
+        
+        // checking if item exists in the array ir not
+        const companyExists = interviewList.find(item => item.companyName == JobCardInfo.companyName);
+    
+        if(!companyExists){
+            interviewList.push(JobCardInfo);
+        }
+        // console.log(interviewList);
+        renderInterview();
+        console.log(JobCardInfo);
+    }
+
+})
